7e9bab1743ee41df800fe7178d19b56749f8d8f2
MrAyush <ayushkgupta1998@gmail.com>
2019-08-22 22:09:42 +0530

    Que 4 done

--- ass1/q4.java
+++ ass1/q4.java
@@ -1,11 +1,63 @@
 class Fraction {
-	int num, den;
-	public Fraction(int num, int den) {
-		this.num = num;
-		this.den = den;
-	}
-	private lcm()
-	Fraction addFraction(Fraction frac) {
-		int n = 
-	}
+    private int num, den;
+
+    public Fraction(int num, int den) {
+        this.num = num;
+        this.den = den;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    private int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    Fraction addFraction(Fraction frac) {
+        int d = lcm(this.den, frac.den);
+        int n1 = (this.den / d) * this.num;
+        int n2 = (frac.den / d) * frac.den;
+        int n = n1 + n2;
+        while(gcd(n, d) != 1) {
+            n = n / gcd(n, d);
+            d = d / gcd(n, d);
+        }
+        return new Fraction(n, d);
+    }
+
+    Fraction subFraction(Fraction frac) {
+        int d = lcm(this.den, frac.den);
+        int n1 = (this.den / d) * this.num;
+        int n2 = (frac.den / d) * frac.den;
+        int n = n1 + n2;
+        while(gcd(n, d) != 1) {
+            n = n / gcd(n, d);
+            d = d / gcd(n, d);
+        }
+        return new Fraction(n, d);
+    }
+
+    Fraction mulFraction(Fraction frac) {
+        int n = this.num * frac.num;
+        int d = this.den * frac.den;
+        while(gcd(n, d) != 1) {
+            n = n / gcd(n, d);
+            d = d / gcd(n, d);
+        }
+        return new Fraction(n, d);
+    }
+
+    Fraction divFraction(Fraction frac) {
+        int n = this.num * frac.den;
+        int d = this.den * frac.num;
+        while(gcd(n, d) != 1) {
+            n = n / gcd(n, d);
+            d = d / gcd(n, d);
+        }
+        return new Fraction(n, d);
+    }
+
 }
